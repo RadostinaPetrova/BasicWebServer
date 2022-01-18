6acd0f3516759fbc792f11c7e7e931aa5cf14106
RadostinaPetrova <rodito0@abv.bg>
2022-01-18 15:40:19 +0200

    Initial commit

---
+++ BasicWebServer.Server/HttpServer.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace BasicWebServer.Server
+{
+    public class HttpServer
+    {
+        private readonly IPAddress ipAddress;
+        private readonly int port;
+        private readonly TcpListener serverListener;
+
+        public HttpServer(string ipAddress, int port)
+        {
+            this.ipAddress = IPAddress.Parse(ipAddress);
+            this.port = port;
+
+            this.serverListener = new TcpListener(this.ipAddress, port);
+        }
+
+        public void Start()
+        {
+            this.serverListener.Start();
+
+            Console.WriteLine($"Server started on port {port}.");
+            Console.WriteLine("Listening for requests...");
+
+            while (true)
+            {
+                var connection = serverListener.AcceptTcpClient();
+
+                var networkStream = connection.GetStream();
+                string content = "Hello from the server!";
+
+                WriteResponse(networkStream, content);
+                
+                connection.Close();
+            }
+        }
+
+        private void WriteResponse(NetworkStream networkStream, string message)
+        {
+            int contentLength = Encoding.UTF8.GetByteCount(message);
+
+            var response = $@"HTTP/1.1 200 OK
+Content-Type: text/plain; charset=UTF-8
+Content-Length: {contentLength}
+
+{message}";
+
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+            networkStream.Write(responseBytes);
+        }
+    }
+}
