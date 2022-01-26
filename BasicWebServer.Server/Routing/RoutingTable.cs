5924f9830e650cc20ef487dd2b15852fa2b9e4e0
RadostinaPetrova <rodito0@abv.bg>
2022-01-26 20:09:29 +0200

    HomeController added

--- BasicWebServer.Server/Routing/RoutingTable.cs
+++ BasicWebServer.Server/Routing/RoutingTable.cs
@@ -6,7 +6,7 @@ namespace BasicWebServer.Server.Routing
 {
     public class RoutingTable : IRoutingTable
     {
-        private readonly Dictionary<Method, Dictionary<string, Response>> routes;
+        private readonly Dictionary<Method, Dictionary<string, Func<Request, Response>>> routes;
 
         public RoutingTable() => this.routes = new()
         {
@@ -16,30 +16,21 @@ namespace BasicWebServer.Server.Routing
             [Method.Delete] = new()
         };
 
-        public IRoutingTable Map(string url, Method method, Response response) => method switch
+        public IRoutingTable Map(Method method, string path, Func<Request, Response> responseFunction)
         {
-            Method.Get => this.MapGet(url, response),
-            Method.Post => this.MapPost(url, response), 
-            _ => throw new InvalidOperationException($"Method '{method}' is not supported.")
-        };
+            Guard.AgainstNull(path, nameof(path));
+            Guard.AgainstNull(responseFunction, nameof(responseFunction));
 
-        public IRoutingTable MapGet(string url, Response response)
-        {
-            Guard.AgainstNull(url, nameof(url));
-            Guard.AgainstNull(response, nameof(response));
+            this.routes[method][path] = responseFunction;
 
-            this.routes[Method.Get][url] = response;
             return this;
         }
 
-        public IRoutingTable MapPost(string url, Response response)
-        {
-            Guard.AgainstNull(url, nameof(url));
-            Guard.AgainstNull(response, nameof(response));
+        public IRoutingTable MapGet(string path, Func<Request, Response> responseFunction)
+          => Map(Method.Get, path, responseFunction);
 
-            this.routes[Method.Post][url] = response;
-            return this;
-        }
+        public IRoutingTable MapPost(string path, Func<Request, Response> responseFunction)
+          => Map(Method.Post, path, responseFunction);
 
         public Response MatchRequest(Request request)
         {
@@ -51,7 +42,8 @@ namespace BasicWebServer.Server.Routing
                 return new NotFoundResponse();
             }
 
-            return this.routes[requestMethod][requestUrl];
+            var responseFunction = this.routes[requestMethod][requestUrl];
+            return responseFunction(request);
         }
     }
 }
--- BasicWebServer/Startup.cs
+++ BasicWebServer/Startup.cs
@@ -1,4 +1,6 @@
-﻿using BasicWebServer.Server;
+﻿using BasicWebServer.Demo.Controllers;
+using BasicWebServer.Server;
+using BasicWebServer.Server.Controllers;
 using BasicWebServer.Server.HTTP;
 using BasicWebServer.Server.Responses;
 using System.Text;
@@ -6,18 +8,6 @@ using System.Web;
 
 public class Startup
 {
-    private const string HtmlForm = @"<form action='/HTML' method='POST'>
-   Name: <input type='text' name='Name'/>
-   Age: <input type='number' name ='Age'/>
-<input type='submit' value ='Save' />
-</form>";
-
-    private const string Downloadform = @"<form action='/Content' method='POST'>
-   <input type='submit' value ='Download Sites Content' /> 
-</form>";
-
-    private const string FileName = "content.txt";
-
     private const string LoginForm = @"<form action='/Login' method='POST'>
    Username: <input type='text' name='Username'/>
    Password: <input type='text' name='Password'/>
@@ -30,22 +20,20 @@ public class Startup
 
     public static async Task Main()
     {
-        await DownloadSitesAsTextFile(Startup.FileName, new string[] { "https://judge.softuni.org/", "https://softuni.org/" });
-
         var server = new HttpServer(
             routes => routes
-            .MapGet("/", new TextResponse("Hello form the server!"))
-            .MapGet("/Redirect", new RedirectResponse("https://softuni.org/"))
-            .MapGet("/HTML", new HtmlResponse(Startup.HtmlForm))
-            .MapPost("/HTML", new TextResponse("", Startup.AddFormDataAction))
-            .MapGet("/Content", new HtmlResponse(Startup.Downloadform))
-            .MapPost("/Content", new TextFileResponse(Startup.FileName))
-            .MapGet("/Cookies", new HtmlResponse("", Startup.AddCookiesAction))
-            .MapGet("/Session", new TextResponse("", Startup.DisplaySessionInfoAction))
-            .MapGet("/Login", new HtmlResponse(Startup.LoginForm))
-            .MapPost("/Login", new HtmlResponse("", Startup.LoginAction))
-            .MapGet("/Logout", new HtmlResponse("", Startup.LogoutAction))
-            .MapGet("/UserProfile", new HtmlResponse("", Startup.GetUserDataAction)));
+            .MapGet<HomeController>("/", c => c.Index())
+            .MapGet<HomeController>("/Redirect", c => c.Redirect())
+            .MapGet<HomeController>("/HTML", c => c.Html())
+            .MapPost<HomeController>("/HTML", c => c.HtmlFormPost())
+            .MapGet<HomeController>("/Content", c => c.Content())
+            .MapPost<HomeController>("/Content", c => c.DownloadContent())
+            .MapGet<HomeController>("/Cookies", c => c.Cookies())
+            .MapGet<HomeController>("/Session", c => c.Session()));
+        //.MapGet<UsersController>("/Login", new HtmlResponse(Startup.LoginForm))
+        //.MapPost<UsersController>("/Login", new HtmlResponse("", Startup.LoginAction))
+        // .MapGet<UsersController>("/Logout", new HtmlResponse("", Startup.LogoutAction))
+        //.MapGet<UsersController>("/UserProfile", new HtmlResponse("", Startup.GetUserDataAction)));
 
         await server.Start();
     }
@@ -81,7 +69,7 @@ public class Startup
         var usernameMatches = request.Form["Username"] == Startup.Username;
         var passwordMatches = request.Form["Password"] == Startup.Password;
 
-        if(usernameMatches && passwordMatches)
+        if (usernameMatches && passwordMatches)
         {
             request.Session[Session.SessionUserKey] = "MyUserId";
             response.Cookies.Add(Session.SessionCookieName, request.Session.Id);
@@ -91,26 +79,6 @@ public class Startup
         else
         {
             bodyText = Startup.LoginForm;
-        }
-
-        response.Body = "";
-        response.Body += bodyText;
-    }
-
-    private static void DisplaySessionInfoAction(Request request, Response response)
-    {
-        var sessionExists = request.Session.ContainsKey(Session.SessionCurrentDateKey);
-
-        var bodyText = "";
-
-        if (sessionExists)
-        {
-            var currentDate = request.Session[Session.SessionCurrentDateKey];
-            bodyText = $"Stored date: {currentDate}!";
-        }
-        else
-        {
-            bodyText = "Current date stored!";
         }
 
         response.Body = "";
@@ -128,65 +96,6 @@ public class Startup
         }
     }
 
-    private static async Task<string> DownloadWebSiteContent(string url)
-    {
-        var httpClient = new HttpClient();
-        using (httpClient)
-        {
-            var response = await httpClient.GetAsync(url);
-
-            var html = await response.Content.ReadAsStringAsync();
-
-            return html.Substring(0, 2000);
-        }
-    }
-
-    private static async Task DownloadSitesAsTextFile(string fileName, string[] urls)
-    {
-        var downloads = new List<Task<string>>();
-        foreach (var url in urls)
-        {
-            downloads.Add(DownloadWebSiteContent(url));
-        }
-
-        var responses = await Task.WhenAll(downloads);
-
-        var responsesString = string.Join(Environment.NewLine + new String('-', 100), responses);
-
-        await File.WriteAllTextAsync(fileName, responsesString);
-    }
-
-    private static void AddCookiesAction(Request request, Response response)
-    {
-        var requestHasCookies = request.Cookies.Any(c => c.Name != Session.SessionCookieName);
-        var bodyText = "";
-
-        if (requestHasCookies)
-        {
-            var cookieText = new StringBuilder();
-            cookieText.AppendLine("<h1>Cookies</h1>");
-
-            cookieText.Append("<table border='1'><tr><th>Name</th><th>Value</th></tr>");
-
-            foreach (var cookie in request.Cookies)
-            {
-                cookieText.Append("<tr>");
-                cookieText.Append($"<td>{HttpUtility.HtmlEncode(cookie.Name)}</td>");
-                cookieText.Append($"<td>{HttpUtility.HtmlEncode(cookie.Value)}</td>");
-                cookieText.Append("</tr>");
-            }
-            cookieText.Append("</table>");
-
-            bodyText = cookieText.ToString();
-        }
-        else
-        {
-            bodyText = "<h1>Cookies set!</h1>";
-            response.Cookies.Add("My-Cookie", "My-Value");
-            response.Cookies.Add("My-Second-Cookie", "My-Second-Value");
-        }
-
-        response.Body = bodyText;
-    }
+    
 }
 
